cd2236211a2973ace79008a4678c597f13a90f30
Jesse Waddell <jesse@Jesses-MBP.attlocal.net>
2017-09-01 10:28:22 -0500

    added partial folder

---
+++ client/app/app.js
@@ -0,0 +1,7 @@
+const app = angular.module('how2', ['ngRoute'])
+
+    app.config(($routeProvider, $locationProvider) => {
+        $locationProvider.hashPrefix('')
+        $routeProvider
+            .when()
+    })
