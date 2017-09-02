6144183e8b09287729d32c947fe1a0979da557d9
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-09-02 13:02:44 -0500

    got some h1 to display on home partial

--- client/app/app.js
+++ client/app/app.js
@@ -3,7 +3,7 @@ const app = angular.module('how2', ['ngRoute'])
     app.config(($routeProvider, $locationProvider) => {
         $locationProvider.hashPrefix('')
         $routeProvider
-            .when('/', {
+            .when('/home', {
                 controller: 'HomeCtrl',
                 templateUrl: '../partials/home.html'
             })
